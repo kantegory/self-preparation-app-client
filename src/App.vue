e9d272ae1f568851f1e57e3960b3595de54601f6
David Dobryakov <davidobryakov@gmail.com>
2021-01-09 18:16:16 +0300

    Use router-view instead of component hardcode

--- src/App.vue
+++ src/App.vue
@@ -38,23 +38,13 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+  name: 'App'
 }
 </script>
